69fceff9cf7bf73940d24508e3cef24b33d4256d
Zaid Alam <zaidalam0731@gmail.com>
2024-02-28 04:08:46 -0500

    Code for Signup

---
+++ controllers/signupController.js
@@ -0,0 +1,28 @@
+const userModel = require("../models/userModel.js");
+
+const signupController = async (req, res) => {
+    console.log("Hiiiii");
+    try {
+        
+        const userdata = {
+            fullName: req.body.fullName,
+            email: req.body.email,
+            phoneNumber: req.body.phoneNumber,
+            password: req.body.password,
+        };
+        const check = await userModel.findOne({ email: userdata.email });
+
+        if (check) {
+            res.json("User Exists");
+        } else {
+            await userModel.create(userdata);
+            res.json("User Does Not Exist");
+        }
+    } catch (error) {
+        console.error(error);
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+module.exports = signupController;
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ require('./db/dbinit.js');
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 7000
 
-app.get("/api",(req,res)=>{
-    res.json({"users" : ["Zaid","Alam"]});
-});
+//Middleware
+app.use(express.json());
+
+
+
+//Controllers
+const signupController=require("./controllers/signupController.js")
+
+
+//Port Details
+const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log("App is running at port " + port);
 });
+  
+
+// Routes
+app.post("/signup",signupController);
