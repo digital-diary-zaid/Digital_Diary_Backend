d1c7895d10eda2fcf3e1e61f199ed2453bc05ed0
Zaid Alam <zaidalam0731@gmail.com>
2024-04-13 17:03:11 -0400

    Added cors part here

--- server.js
+++ server.js
@@ -5,7 +5,27 @@ const expressSession = require("express-session");
 const app = express();
 const authMiddleware = require("./middleware/authMiddleware");
 const cors=require('cors');
-app.use(cors());
+
+
+
+// Define the frontend URL (origin) allowed to access the backend
+const allowedOrigins = ['https://diztaldiary.netlify.app'];
+
+// Configure CORS with options
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true); // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS')); // Reject the request
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+};
+
+// Use the CORS middleware with the configured options
+app.use(cors(corsOptions));
 
 
 //Middleware
@@ -43,7 +63,7 @@ app.listen(port, () => {
 
 // Routes
 app.post("/signup",signupController);
-app.post("/login",loginController);
+app.post("/api/login",loginController);
 app.post("/addNote",authMiddleware,saveNoteController);
 app.get("/api/getNotes",authMiddleware,getNotesController);
 app.get("/logout",logoutController);
