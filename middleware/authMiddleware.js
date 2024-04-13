84b9b971b681a1389ed4733be84019021f5dcd8d
Zaid Alam <zaidalam0731@gmail.com>
2024-04-13 18:55:39 -0400

    Added session storage cheking for the server

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const authMiddleware = (req, res, next) => {
     if (!req.session.userId) {
+      console.log("User", req.session.userId);
       return res.json({ message: "Unauthorized" });
     }
     next();
