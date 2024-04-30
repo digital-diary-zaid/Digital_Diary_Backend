c830bdd19ee623651b11c5fb24efdabcfa3a05e1
Zaid Alam <zaidalam0731@gmail.com>
2024-04-30 01:40:20 -0400

    Added checking for no notes on initalt signup

--- controllers/getNotesController.js
+++ controllers/getNotesController.js
@@ -2,24 +2,22 @@ const noteModel = require("../models/noteModel.js");
 const userModel = require("../models/userModel.js");
 
 const getNotesController = async (req, res) => {
+  try {
+    const loggedInUserId = req.session.userId; // Assuming you have the user ID in the request object
+    console.log("loggedInUserId", loggedInUserId);
+    // Check if the user has notes
+    let userNotes = await noteModel.findOne({ userId: loggedInUserId });
 
-    try {
-        const loggedInUserId = req.session.userId; // Assuming you have the user ID in the request object
-        
-        // Check if the user has notes
-        let userNotes = await noteModel.findOne({ userId: loggedInUserId });
-
-        // If the user does not have any notes
-        if (!userNotes.notes || userNotes.notes.length <=0) {
-            return res.json({ message: "No notes" });
-        } else {
-            return res.json({ message: "Successfull", userNotes });
-        }
-
-    } catch (error) {
-        console.error("Error saving note:", error);
-        return res.status(500).json({ message: "Error saving note" });
+    // If the user does not have any notes
+    if (!userNotes?.notes || userNotes?.notes?.length <= 0) {
+      return res.json({ message: "No notes" });
+    } else {
+      return res.json({ message: "Successfull", userNotes });
     }
-}
+  } catch (error) {
+    console.error("Error getting note:", error);
+    return res.status(500).json({ message: "Error getting note" });
+  }
+};
 
 module.exports = getNotesController;
